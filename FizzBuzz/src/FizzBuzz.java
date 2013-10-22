a7f9766e960135767c05552fc227f0fef8660a7a
William Kennedy <williamkennedy@William-Kennedys-MacBook-Pro.local>
2013-10-22 14:02:32 +0100

    Uploading Files- Test
    Test

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,20 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		for (int i = 1; i<=100; i++) {
+			if (i % 3 == 0) {
+				System.out.println("Fizz");
+			}
+			if (i % 5 == 0) {
+				System.out.println("Buzz");	
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+		}
+
+	}
+
+
